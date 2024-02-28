0ad9edf0f723f80090c750318f5dfdc2ec26fc97
Michael Whapples <software@mdw.onl>
2024-02-28 18:31:12 +0000

    Create a data class for settings profiles

--- src/convert2ebrf/utils.py
+++ src/convert2ebrf/utils.py
@@ -4,8 +4,24 @@
 # Convert2EBRF is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # Convert2EBRF is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with Convert2EBRF. If not, see <https://www.gnu.org/licenses/>.
+from dataclasses import dataclass
 
 from PySide6.QtCore import QRunnable, Slot
+
+from brf2ebrf.common import PageNumberPosition
+
+
+@dataclass(frozen=True)
+class SettingsProfile:
+    id: str
+    name: str
+    detect_runningheads: bool
+    cells_per_line: int
+    lines_per_page: int
+    odd_bpn_position: PageNumberPosition
+    even_bpn_position: PageNumberPosition
+    odd_ppn_position: PageNumberPosition
+    even_ppn_position: PageNumberPosition
 
 
 class RunnableAdapter(QRunnable):
