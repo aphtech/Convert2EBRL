5eab618a47bfa633acab0e18cec36a3b5c2c50d6
Michael Whapples <software@mdw.onl>
2024-03-04 11:17:45 +0000

    Add UUID for identifying/storing profiles.

--- src/convert2ebrf/settings/__init__.py
+++ src/convert2ebrf/settings/__init__.py
@@ -4,3 +4,23 @@
 # Convert2EBRF is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # Convert2EBRF is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with Convert2EBRF. If not, see <https://www.gnu.org/licenses/>.
+from dataclasses import dataclass, field
+from uuid import UUID, uuid5
+from brf2ebrf.common import PageNumberPosition
+
+SETTINGS_PROFILE_NAMESPACE = UUID("8c4d55b1-98c0-484c-8677-4a019353c3aa")
+
+
+@dataclass(frozen=True)
+class SettingsProfile:
+    id: UUID = field(init=False)
+    name: str
+    detect_runningheads: bool = True
+    cells_per_line: int = 40
+    lines_per_page: int = 25
+    odd_bpn_position: PageNumberPosition = PageNumberPosition.BOTTOM_RIGHT
+    even_bpn_position: PageNumberPosition = PageNumberPosition.NONE
+    odd_ppn_position: PageNumberPosition = PageNumberPosition.TOP_RIGHT
+    even_ppn_position: PageNumberPosition = PageNumberPosition.NONE
+    def __post_init__(self):
+        object.__setattr__(self, "id", uuid5(SETTINGS_PROFILE_NAMESPACE, self.name))
