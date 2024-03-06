2ce43c6e0018aa43015894ccb75e08c126301377
Michael Whapples <software@mdw.onl>
2024-03-06 17:31:34 +0000

    Refactoring to prepare for remembering settings.

--- src/convert2ebrf/utils.py
+++ src/convert2ebrf/utils.py
@@ -43,16 +43,21 @@ def save_settings_profiles(profiles: Iterable[SettingsProfile], clear_existing: 
         settings.clear()
     for profile in profiles:
         settings.begin_group(str(profile.id))
-        settings.set_value(PROFILE_NAME, profile.name)
-        settings.set_value(PROFILE_DETECT_RUNNINGHEADS, profile.detect_runningheads)
-        settings.set_value(PROFILE_CELLS_PER_LINE, profile.cells_per_line)
-        settings.set_value(PROFILE_LINES_PER_PAGE, profile.lines_per_page)
-        settings.set_value(PROFILE_ODD_BPN_POSITION, profile.odd_bpn_position.name)
-        settings.set_value(PROFILE_EVEN_BPN_POSITION, profile.even_bpn_position.name)
-        settings.set_value(PROFILE_ODD_PPN_POSITION, profile.odd_ppn_position.name)
-        settings.set_value(PROFILE_EVEN_PPN_POSITION, profile.even_ppn_position.name)
+        save_settings_profile(settings, profile)
         settings.end_group()
     settings.sync()
+
+
+def save_settings_profile(settings, profile):
+    settings.set_value(PROFILE_NAME, profile.name)
+    settings.set_value(PROFILE_DETECT_RUNNINGHEADS, profile.detect_runningheads)
+    settings.set_value(PROFILE_CELLS_PER_LINE, profile.cells_per_line)
+    settings.set_value(PROFILE_LINES_PER_PAGE, profile.lines_per_page)
+    settings.set_value(PROFILE_ODD_BPN_POSITION, profile.odd_bpn_position.name)
+    settings.set_value(PROFILE_EVEN_BPN_POSITION, profile.even_bpn_position.name)
+    settings.set_value(PROFILE_ODD_PPN_POSITION, profile.odd_ppn_position.name)
+    settings.set_value(PROFILE_EVEN_PPN_POSITION, profile.even_ppn_position.name)
+
 
 def load_settings_profiles() -> Iterable[SettingsProfile]:
     profiles = []
@@ -60,19 +65,23 @@ def load_settings_profiles() -> Iterable[SettingsProfile]:
     profile_ids = settings.child_groups()
     for profile_id in profile_ids:
         settings.begin_group(profile_id)
-        name = settings.value(PROFILE_NAME, DEFAULT_SETTINGS_PROFILE.name)
-        if name == DEFAULT_SETTINGS_PROFILE.name:
-            # Skip not valid, probably should log
-            settings.end_group()
-            continue
-        detect_runningheads = settings.value(PROFILE_DETECT_RUNNINGHEADS, DEFAULT_SETTINGS_PROFILE.detect_runningheads, type=bool)
-        cpl = settings.value(PROFILE_CELLS_PER_LINE, DEFAULT_SETTINGS_PROFILE.cells_per_line, type=int)
-        lpp = settings.value(PROFILE_LINES_PER_PAGE, DEFAULT_SETTINGS_PROFILE.lines_per_page, type=int)
-        odd_bpn = PageNumberPosition[settings.value(PROFILE_ODD_BPN_POSITION, DEFAULT_SETTINGS_PROFILE.odd_bpn_position)]
-        even_bpn = PageNumberPosition[settings.value(PROFILE_EVEN_BPN_POSITION, DEFAULT_SETTINGS_PROFILE.even_bpn_position)]
-        odd_ppn = PageNumberPosition[settings.value(PROFILE_ODD_PPN_POSITION, DEFAULT_SETTINGS_PROFILE.even_ppn_position)]
-        even_ppn = PageNumberPosition[settings.value(PROFILE_EVEN_PPN_POSITION, DEFAULT_SETTINGS_PROFILE.even_ppn_position)]
-        profile = SettingsProfile(name=name, detect_runningheads=detect_runningheads, cells_per_line=cpl, lines_per_page=lpp, odd_bpn_position=odd_bpn, even_bpn_position=even_bpn, odd_ppn_position=odd_ppn, even_ppn_position=even_ppn)
-        profiles.append(profile)
+        profile = load_settings_profile(settings)
+        if profile.name != DEFAULT_SETTINGS_PROFILE.name:
+            profiles.append(profile)
         settings.end_group()
     return profiles
+
+
+def load_settings_profile(settings):
+    name = settings.value(PROFILE_NAME, DEFAULT_SETTINGS_PROFILE.name)
+    detect_runningheads = settings.value(PROFILE_DETECT_RUNNINGHEADS, DEFAULT_SETTINGS_PROFILE.detect_runningheads,
+                                         type=bool)
+    cpl = settings.value(PROFILE_CELLS_PER_LINE, DEFAULT_SETTINGS_PROFILE.cells_per_line, type=int)
+    lpp = settings.value(PROFILE_LINES_PER_PAGE, DEFAULT_SETTINGS_PROFILE.lines_per_page, type=int)
+    odd_bpn = PageNumberPosition[settings.value(PROFILE_ODD_BPN_POSITION, DEFAULT_SETTINGS_PROFILE.odd_bpn_position)]
+    even_bpn = PageNumberPosition[settings.value(PROFILE_EVEN_BPN_POSITION, DEFAULT_SETTINGS_PROFILE.even_bpn_position)]
+    odd_ppn = PageNumberPosition[settings.value(PROFILE_ODD_PPN_POSITION, DEFAULT_SETTINGS_PROFILE.even_ppn_position)]
+    even_ppn = PageNumberPosition[settings.value(PROFILE_EVEN_PPN_POSITION, DEFAULT_SETTINGS_PROFILE.even_ppn_position)]
+    return SettingsProfile(name=name, detect_runningheads=detect_runningheads, cells_per_line=cpl,
+                           lines_per_page=lpp, odd_bpn_position=odd_bpn, even_bpn_position=even_bpn,
+                           odd_ppn_position=odd_ppn, even_ppn_position=even_ppn)
