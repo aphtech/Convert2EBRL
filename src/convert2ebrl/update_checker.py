acd346b6d45c7fdeb0a6a4015bb9a11d499c55ee
Michael Whapples <mwhapples@aph.org>
2025-05-13 15:55:29 +0100

    Allow update checker to report updates, no updates and error.

--- src/convert2ebrl/update_checker.py
+++ src/convert2ebrl/update_checker.py
@@ -6,14 +6,17 @@
 # You should have received a copy of the GNU General Public License along with Convert2EBRL. If not, see <https://www.gnu.org/licenses/>.
 import re
 
-from PySide6.QtCore import QObject, QUrl, Slot, Signal
+from PySide6.QtCore import QObject, QUrl, Slot, Signal, QCoreApplication
 from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from packaging.version import Version
 
 _APP_VERSION_RE = re.compile("^app.version[ \t]*[ \t:=][ \t]*(.*)(?:\n|$)", flags=re.MULTILINE)
 
 class UpdateChecker(QObject):
     checkingForUpdates = Signal()
     updateAvailable = Signal(str)
+    noUpdateAvailable = Signal()
+    errorOccurred = Signal(str)
     def __init__(self, parent: QObject|None = None):
         super().__init__(parent)
         self.network_manager = QNetworkAccessManager(self)
@@ -23,14 +26,21 @@ class UpdateChecker(QObject):
         self.reply = self.network_manager.get(QNetworkRequest(update_url))
         self.reply.readyRead.connect(self.on_ready_read)
         self.reply.finished.connect(self.on_finished)
+        self.reply.errorOccurred.connect(self.on_error)
     @Slot()
     def on_ready_read(self):
         if self.reply:
             if self.reply.error() == QNetworkReply.NetworkError.NoError:
                 response_text = str(self.reply.readAll(), "utf-8")
                 if m := _APP_VERSION_RE.search(response_text):
-                    print(m.group(1))
+                    if Version(QCoreApplication.applicationVersion()) < Version(m.group(1)):
+                        print(m.group(1))
+                        self.updateAvailable.emit(m.group(1))
     @Slot()
     def on_finished(self):
         if self.reply:
             self.reply.deleteLater()
+    @Slot(QNetworkReply.NetworkError)
+    def on_error(self, _: QNetworkReply.NetworkError):
+        if self.reply:
+            self.errorOccurred.emit(self.reply.errorString())
