7c59684c3807f19a64ff926e3990140456e3696d
Michael Whapples <mwhapples@aph.org>
2025-05-13 16:45:59 +0100

    Initial help menu item for check for updates.

--- src/convert2ebrl/main_window.py
+++ src/convert2ebrl/main_window.py
@@ -5,9 +5,12 @@
 # Convert2EBRL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with Convert2EBRL. If not, see <https://www.gnu.org/licenses/>.
 
+from PySide6.QtCore import QUrl
+from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QMainWindow
 
 from convert2ebrl.brf_to_ebrf import Brf2EbrfWidget
+from convert2ebrl.update_checker import UpdateChecker
 
 
 class MainWindow(QMainWindow):
@@ -15,3 +18,9 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Convert BRF to eBraille")
         self.setCentralWidget(Brf2EbrfWidget())
+        update_checker = UpdateChecker(self)
+        update_check_action = QAction("Check for updates", self)
+        update_check_action.triggered.connect(lambda _: update_checker.check_for_update(QUrl("https://download.brailleblaster-ng.app/metadata.properties")))
+        menu = self.menuBar()
+        help_menu = menu.addMenu("Help")
+        help_menu.addAction(update_check_action)
